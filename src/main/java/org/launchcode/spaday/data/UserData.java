f84a74c2969fcd4bbd78f487f970e5e5d16b3378
unknown <ryjwink@gmail.com>
2024-01-25 13:46:03 -0600

    studio for class 12

---
+++ src/main/java/org/launchcode/spaday/data/UserData.java
@@ -0,0 +1,25 @@
+package org.launchcode.spaday.data;
+
+import org.launchcode.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserData {
+
+List<User> users= new ArrayList<User>();
+
+public void add (User user) {
+    users.add(user);
+}
+
+public List<User> getAll() {
+   return users;
+}
+
+public user getById (int ID) {
+ if (Users user: users)
+}
+
+
+}
